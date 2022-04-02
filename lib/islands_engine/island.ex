a4402c4ad7fc7333f5d197e62bbf9050dc743d78
Tim Löfström <lo.tim@hotmail.com>
2022-04-02 07:53:58 +0200

    update

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -35,4 +35,16 @@ defmodule IslandsEngine.Island do
         {:halt, {:error, :invalid_coordinate}}
     end
   end
+
+  def overlaps?(existing_island, new_island) do
+    not MapSet.disjoint?(existing_island.coordinates, new_island.coordinates)
+  end
+
+  def guess(island, coordinate) do
+    case MapSet.member?(island.coordinates, coordinate) do
+      true -> hit_coordinates = MapSet.put(island.hit_coordinates, coordinate)
+      {:hit, %{island | hit_coordinates: hit_coordinates}}
+      false -> :miss
+    end
+  end
 end
