124870e62c6aa93624c8abc33098212e405f4e92
Tim Löfström <tim.lofstrom.danielsson@lm.se>
2022-04-06 21:56:05 +0200

    update

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -1,13 +1,14 @@
 defmodule IslandsEngine.Game do
+  alias IslandsEngine.{Board, Guesses, Rules}
   use GenServer
 
-  def handle_info(:first, state) do
-    IO.puts("Matching on :first")
-    {:noreply, state}
+  def start_link(name) when is_binary(name) do
+    GenServer.start_link(__MODULE__, name, [])
   end
 
-  def handle_call(:demo_call, _from, state) do
-    {:reply, state, state}
+  def init(name) do
+    player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
+    player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
+    {:ok, %{player1: player1, player2: player2, rules: %Rules{}}}
   end
-
 end
